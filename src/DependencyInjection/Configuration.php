c851c7dc642a4409a508500e97e0f6187827161c
Tom Graham <tompedals@users.noreply.github.com>
2015-06-06 10:31:54 +0100

    Corrected config root element.

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -10,7 +10,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('emoji');
+        $rootNode = $treeBuilder->root('hey_update_emoji');
 
         $rootNode
             ->children()
